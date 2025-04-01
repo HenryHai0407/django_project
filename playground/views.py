1c35a79ea884a56bfda9c4a5fcfdf6208b9a0096
HenryHai0407 <hoanghai15100@gmail.com>
2025-04-01 23:51:37 +0300

    create debugging log with launch.json and customize port 9000

--- playground/views.py
+++ playground/views.py
@@ -2,5 +2,13 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
+
+def calculate():
+    x = 100
+    y = 200
+    return x
+
+
 def say_hello(request):
-    return HttpResponse('Hello this World!!!!!')
+    a = calculate()
+    return render(request,'hello.html',{'name':'Harry Potter'})
